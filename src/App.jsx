433eb2832e2c0bee53fad99247a204cff98177cc
Anna Cristina <annacristinaferreiraalves@gmail.com>
2025-03-01 17:05:06 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,17 @@ import ProtectedRoute from './Components/Helper/ProtectedRoute.jsx';
 
 const App = () => {
   return (
-    <div>
+    <div className='App'>
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='login/*' element={<Login />} />
-            <Route path='conta/*' element={<ProtectedRoute> <User /> </ProtectedRoute>}/>
-          </Routes>
+          <main className='AppBody'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='login/*' element={<Login />} />
+              <Route path='conta/*' element={<ProtectedRoute> <User /> </ProtectedRoute>} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
