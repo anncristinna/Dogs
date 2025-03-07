4a4006d8f37136a1f66e129b31a025529ae82a7e
Anna Cristina <annacristinaferreiraalves@gmail.com>
2025-03-07 00:19:07 -0300

    loading animacao

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Feed from './Feed/Feed'
+import Head from './Helper/Head'
+import Loading from './Helper/Loading'
 
 const Home = () => {
   return (
     <section className='container mainContainer'>
-      <Feed/>
+      <Head title='Home' description='Feed com foto de diversos usuários' />
+      <Feed />
     </section>
   )
 }
